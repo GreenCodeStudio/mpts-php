75b904522bcc7378a7a520e2267c484977e48708
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-03-19 22:31:33 +0100

    phpstan

--- src/Nodes/TExpressionText.php
+++ src/Nodes/TExpressionText.php
@@ -3,9 +3,12 @@
 namespace MKrawczyk\Mpts\Nodes;
 
 use MKrawczyk\Mpts\Environment;
+use MKrawczyk\Mpts\Nodes\Expressions\TEExpression;
 
 class TExpressionText extends TNode
 {
+    public ?TEExpression $expression;
+
     public function execute(Environment $env):\DOMText
     {
         return $env->document->createTextNode($this->expression->execute($env));
