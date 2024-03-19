c74a3dd4125eea8aa59d40b090999821448f1a78
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-03-19 23:01:55 +0100

    phpstan

--- src/Nodes/TExpressionSubnode.php
+++ src/Nodes/TExpressionSubnode.php
@@ -3,9 +3,12 @@
 namespace MKrawczyk\Mpts\Nodes;
 
 use MKrawczyk\Mpts\Environment;
+use MKrawczyk\Mpts\Nodes\Expressions\TEExpression;
 
 class TExpressionSubnode extends TNode
 {
+    public TEExpression $expression;
+
     public function execute(Environment $env):\DOMDocumentFragment
     {
         $frag=$env->document->createDocumentFragment();
