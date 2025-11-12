54a91e36f73260aca5a55edaded982aedcb7ed44
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-11-12 16:46:24 +0100

    fix

--- src/Nodes/TExpressionText.php
+++ src/Nodes/TExpressionText.php
@@ -9,8 +9,16 @@ class TExpressionText extends TNode
 {
     public ?TEExpression $expression;
 
-    public function execute(Environment $env):\DOMText
+    public function execute(Environment $env): \DOMText
     {
-        return $env->document->createTextNode(print_r($this->expression->execute($env),1));
+        $value = $this->expression->execute($env);
+        if (is_string($value)) {
+            $string = $value;
+        } else if (is_object($value) && method_exists($value, '__toString')) {
+            $string = $value->__toString();
+        } else {
+            $string = print_r($value, 1);
+        }
+        return $env->document->createTextNode($string);
     }
 }
